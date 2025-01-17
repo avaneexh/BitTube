e8a5970f91173691ff32345318108be5c31bfb7b
Avaneesh Gupta <avaneesh.gupta5@gmail.com>
2025-01-17 22:11:23 +0530

    added api url

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(
     cors({
         origin: [
-            "https://streamify-video-streaming.vercel.app",
+            "https://bittube-backend.onrender.com",
             "http://localhost:5173",
         ],
         credentials: true,
